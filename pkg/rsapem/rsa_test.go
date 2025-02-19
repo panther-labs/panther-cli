2e5bec5a7de50e494de3d6e9b90f13f76b670fe8
Tim Schumacher <tim.schumacher@panther.com>
2025-02-19 13:23:09 -0500

    Remove enterprise copyright header

--- pkg/rsapem/rsa_test.go
+++ pkg/rsapem/rsa_test.go
@@ -1,14 +1,5 @@
 package rsapem
 
-/**
- * Copyright (C) 2022 Panther Labs, Inc.
- *
- * The Panther SaaS is licensed under the terms of the Panther Enterprise Subscription
- * Agreement available at https://panther.com/enterprise-subscription-agreement/.
- * All intellectual property rights in and to the Panther SaaS, including any and all
- * rights to access the Panther SaaS, are governed by the Panther Enterprise Subscription Agreement.
- */
-
 import (
 	"crypto/rsa"
 	"crypto/x509"
