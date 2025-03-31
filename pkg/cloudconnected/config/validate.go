bbae3b4331000cf825e14b44e105c90d3dff3e19
Lucy Suddenly <lucy.suddenly@panther.com>
2025-03-31 11:03:32 -0700

    chore: true up supported regions

--- pkg/cloudconnected/config/validate.go
+++ pkg/cloudconnected/config/validate.go
@@ -59,11 +59,14 @@ func validatePantherRegion(fl validator.FieldLevel) bool {
 		"ap-southeast-2",
 		"ca-central-1",
 		"eu-central-1",
+		"eu-north-1",
 		"eu-west-1",
 		"eu-west-2",
 		"eu-west-3",
+		"sa-east-1",
 		"us-east-1",
 		"us-east-2",
+		"us-west-1",
 		"us-west-2",
 	}
 
