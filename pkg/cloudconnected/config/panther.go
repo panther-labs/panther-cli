bcc7abdd43e4413f34660f586311317ca95a5ba8
Zac Brown <zacbrown@users.noreply.github.com>
2025-06-26 09:23:51 -0700

    Wrong validation used for IpAddressAllowList.
    Signed-off-by: Zac Brown <zacbrown@users.noreply.github.com>

--- pkg/cloudconnected/config/panther.go
+++ pkg/cloudconnected/config/panther.go
@@ -8,5 +8,5 @@ type PantherAccountConfig struct {
 	DesiredAccountName string   `yaml:"DesiredAccountName" validate:"required"`
 	Edition            string   `yaml:"Edition"            validate:"required,oneof=ENTERPRISE ESSENTIALS"` // if PantherEdition==Enterprise, SnowflakeEdition must be Enterprise
 	Region             string   `yaml:"Region"             validate:"required,validPantherRegion"`          // this informs which Snowflake region we use, currently AWS-only
-	IpAddressAllowList []string `yaml:"IpAddressAllowList" validate:"omitempty,dive,ip"`                    // Optional list of allowed IP addresses
+	IpAddressAllowList []string `yaml:"IpAddressAllowList" validate:"omitempty,dive,cidr"`                  // Optional list of allowed IP addresses
 }
