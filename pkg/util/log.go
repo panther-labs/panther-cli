e3dab65a2617fc99d26692948d81d15dd7ed8e97
nskobov <93276498+nskobov@users.noreply.github.com>
2026-01-26 14:52:28 -0600

    colorize cert validation steps for emphasis (#105)

--- pkg/util/log.go
+++ pkg/util/log.go
@@ -1,9 +1,26 @@
 package util
 
 import (
+	"fmt"
 	"log"
 	"os"
 )
+
+const (
+	red = "\033[0;31m"
+	end = "\033[0m"
+)
+
+func LogRedf(format string, args ...any) {
+	formattedMsg := fmt.Sprintf(format, args...)
+	coloredMsg := fmt.Sprintf("%s%s%s", red, formattedMsg, end)
+	log.Print(coloredMsg)
+}
+
+func LogRedln(msg string) {
+	coloredMsg := fmt.Sprintf("%s%s%s", red, msg, end)
+	log.Println(coloredMsg)
+}
 
 // LogDebugf logs a message if the DEBUG environment variable is set.
 func LogDebugf(msg string, args ...interface{}) {
